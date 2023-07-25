452ef925011da0b8f5763ddfdf23c2639b3db4fb
Gabrielshow <adefelashogbanmu@gmail.com>
2023-07-25 15:46:18 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css'
+
+const App = () => {
+  return (
+    <>
+      <div class="context">
+        <h1>Mappollonos</h1>
+      </div>
+      <div class="area">
+        <ul class="circles">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default App
